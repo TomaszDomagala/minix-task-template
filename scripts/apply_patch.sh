a1fd7612bfb1da35021213cf263630df99e25584
Tomasz Domagała <tomasz.domagala.contact@gmail.com>
2021-04-27 17:08:23 +0200

    Rename scripts

---
+++ scripts/apply_patch.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Source: https://stackoverflow.com/a/24597941
+function fail {
+    printf '%s\n' "$1" >&2  # Send message to stderr.
+    exit "${2-1}"  # Return a code specified by $2 or 1 by default.
+}
+
+# This script's directory.
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# Read config file.
+. "${dir}/.config"
+
+# Required commands.
+commands=("ssh" "scp")
+for c in "${commands[@]}"
+do
+if ! command -v "${c}" &> /dev/null
+then
+    fail "${c} could not be found"
+fi
+done
+
+cd "${dir}/.." || fail "could not cd to / dir"
+file="${username}.patch"
+
+if [[ ! -f "$file" ]]; then
+    fail "could not find $file. use craete_patch.sh to generate .patch file"
+fi
+
+scp "$file" root@localhost:/ -p "$ssh_port" || fail "could not copy file to the machine"
+ssh root@localhost:/ -p "$ssh_port" "cd /; apply -p1 < $file" || "could not apply changes"
